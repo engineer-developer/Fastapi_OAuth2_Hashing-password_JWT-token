2fd27e6431bb1264455b9c3c0aedc881d71d4af1
Suslov Alexey <suslov.lexx@gmail.com>
2024-12-07 22:38:35 +0500

    del column salt from Password table

--- src/dto/passwords/utils.py
+++ src/dto/passwords/utils.py
@@ -1,22 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.auth.utils import make_hashed_password
+from src.auth.utils import get_password_hash
 from src.dao.models import Password
 from src.dto.passwords.schemas import PasswordCreateSchema
 
 
 async def create_password_instance(
-    session: AsyncSession,
     password: str,
 ) -> Password:
     """Create instance of Password"""
 
-    hashed_password, salt = await make_hashed_password(
-        password=password,
-    )
+    hashed_password = await get_password_hash(password)
 
     new_password_schema = PasswordCreateSchema(
         hashed_password=hashed_password,
-        salt=salt,
     )
     return Password(**new_password_schema.model_dump())
